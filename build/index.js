3d26c24e11787a5649c182222d8dc3d454d0e692
Andy Dayton <amdayton@gmail.com>
2022-01-02 11:47:09 -0600

    remove build dir from repo

--- build/index.js
+++
@@ -1,9 +0,0 @@
-import {helloWorld} from './hello-world.js';
-import confetti from './_snowpack/pkg/canvas-confetti.js';
-
-helloWorld();
-
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
